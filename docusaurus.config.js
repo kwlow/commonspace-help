461f3a919cff78ba5ae205654ce1a88b643226b7
KW Low <kw@common.space>
2026-06-19 18:55:39 -0700

    docs: added google analytics measurement id

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,6 +45,10 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-PMYDFDZFGR',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
